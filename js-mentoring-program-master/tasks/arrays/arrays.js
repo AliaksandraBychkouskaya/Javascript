d409d4dde8315d7558e0b972c4b7e061d9b61f59
Aliaksandra Bychkouskaya <Aliaksandra_bychkouskaya@epam.com>
2020-04-09 16:26:45 +0300

    string+arrays

--- js-mentoring-program-master/tasks/arrays/arrays.js
+++ js-mentoring-program-master/tasks/arrays/arrays.js
@@ -1,57 +1,101 @@
 /**
- * 1. Write a method to reverse a string; Function should return the string 'This is not a string'
- * if a number is passed
-* @param {string}
-*/
-function reverseString(str) {
-}
-
-/**
  * return an array of characters names
+ * @param {Array} chars
+ * @return {Array} - char names
  */
 function getCharactersNames(chars) {
+    let arr = [];
+    for (let i in chars){
+        arr.push(chars[i].name);
+    }
+    console.log (arr);
+    return arr;
 }
 
 /**
  * print (console.log) ids of all characters
+ * @param {Array} chars
  */
 function printCharacterNames(chars) {
+    for (let i in chars){
+        console.log(chars[i].name);
+    }
+
 }
 
 /**
  * return an array of non-human characters
+ * @param {Array} chars
+ * @return {Array} - non human characters
  */
 function getNonHumanCharacters(chars) {
+    let arr = [];
+    for (let i in chars){
+        if (chars[i].species !== "Human") {
+            arr.push(chars[i]);
+        }
+    }
+    return arr;
 }
 
 /**
  * return info about character named 'Jerry Smith'
+ * @param {Array} chars
+ * @return {Object} - Jerry object
  */
 function getJerryInfo(chars) {
+    for (let i in chars){
+        if (chars[i].name === "Jerry Smith")
+        return chars[i];
+    }
 }
 
 /**
  * check if all characters are human. return true if all, false if not
+ * @param {Array} chars
+ * @return {boolean}
  */
 function isAllHuman(chars) {
+    for ( let i in chars) {
+        if (chars[i].species !== "Human")
+        return false;
+    }
+    return true;
 }
 
 /**
  * check if there are any Fish-Person characters. return true if any, false if not
+ * @param {Array} chars
+ * @return {boolean}
  */
 function isAnyFishPerson(chars) {
+    for ( let i in chars) {
+
+        if (chars[i].type === "Fish-Person")
+        return true;
+    }
+    return false;
 }
 
 /**
  * 1. Write a method to find an index of minimal item from an array;
-* @param {array}
-*/
+ * @param {Array} arr
+ * @return {number} - minimum element index
+ */
 function minItem(arr) {
     //PLACE YOUR CODE HERE
-  }
+    let min = arr[0];
+    let minindex = 0;
+    for (let i in arr){
+        if (arr[i] < min){
+            minindex = i;
+            min = arr [i];
+        }
+    }
+    return minindex;
+}
 
 module.exports = {
-    reverseString,
     getCharactersNames,
     printCharacterNames,
     getNonHumanCharacters,
--- js-mentoring-program-master/tasks/strings/strings.js
+++ js-mentoring-program-master/tasks/strings/strings.js
@@ -1,31 +1,93 @@
 /**
+ * 1. Write a method to reverse a string; Function should return the string 'This is not a string'
+ * if a number is passed
+ * @param {string} str
+ */
+function reverseString(str) {
+  if (typeof str !== "string") {
+    return 'This is not a string';
+  } else {
+    str = str.split("");
+    str = str.reverse();
+    str = str.join("");
+    return str;
+  }
+}
+//console.log(reverseString("abcdefg"));
+//console.log(reverseString(12345));
+/**
  * 1. Calculate a century by given year
  * if a number is passed
-* @param {number}
-*/
+ * @param {number} year
+ */
 function centuryFromYear(year) {
-  
+  if (typeof year !== "number") {
+    return 'It is not a number';
+  } 
+  else
+   {
+      return Math.ceil(year/100);}
 }
-
+//console.log(centuryFromYear("sddfb"));
+//console.log(centuryFromYear(5));
+//console.log(centuryFromYear(100));
+//console.log(centuryFromYear(1502));
+//console.log(centuryFromYear(2000));
+//console.log(centuryFromYear(2020));
 /**
  * Calculate count of the provided char in the string
- * @param {string}
- * @param {string}
+ * @param {string} str
+ * @param {string} char
  */
-function strCount(str, char) { 
+function strCount(str, char) {
+  const regex = new RegExp(char, 'g');
+  let result = str.match(regex);
+  return result ? result.length : 0;
 }
+console.log (strCount("aliaksandra", "a"));
 
 /**
- * We need to reduce the length of the string or truncate it if it is longer 
+ * We need to reduce the length of the string or truncate it if it is longer
  * than the given maximum length specified and add ... to the end. If it is not that long then we keep it as is.
- * @param {string} - the initial string
- * @param {num} - by wht amount of chars it should be truncated
+ * @param {string} str - the initial string
+ * @param {num} num - by wht amount of chars it should be truncated
  */
-function truncateString(str,num) { 
+function truncateString(str, num) {
+  if (str.length <= num)
+    return str;
+  else 
+    return str.slice(0,num)+"...";
 }
+console.log(truncateString ("My name is Alex", 10));
 
+/**
+ * replace 10 with 'ten' word
+ * @param {string} text - input text
+ * @return {string} - updated text
+ * @example
+ * console.log(replace10("231054")) // 23ten54
+ */
+function replace10(text) {
+  return text.replace("10","ten");
+}
+console.log(replace10 ("31054"));
+/**
+ * replace value in square brackets with CONFIDENTIAL
+ * @param text - input text
+ * @return {string} - updated string
+ * @example
+ * console.log(replaceConfidential("lorem [ipsum] si dolor")) // lorem [CONFIDENTIAL] si dolor
+ */
+function replaceConfidential(text) {
+  const regex = /\[(.*?)\]/g;
+  return text.replace (  regex, "[CONFIDENTIAL]")
+}
+console.log(replaceConfidential("lorem [ipsum] si dolor"));
 module.exports = {
+  reverseString,
   centuryFromYear,
   strCount,
-  truncateString
+  truncateString,
+  replace10,
+  replaceConfidential
 };
