ef033e9c9ef5843c8d90615895e22832f69dd00a
Aliaksandra Bychkouskaya <Aliaksandra_bychkouskaya@epam.com>
2020-04-18 15:42:45 +0300

    objects+classes

--- js-mentoring-program-master/tasks/classes/objects.js
+++ js-mentoring-program-master/tasks/classes/objects.js
@@ -2,19 +2,31 @@
  * create object person with firstName and lastName properties
  * implement method getFullName that returns full name
  */
-const person = //put you object here
+const person = {
+    firstName: "Aliaksandra",
+    lastName: "Peshko",
+    getFullName: function (){
+        return this.firstName + ' '+ this.lastName;
+    }
+
+}//put you object here
 
 /**
  * create object student that has property grade and method getGrade
  * set person as prototype of student
  */
-const student = //put you object here
+const student = Object.create(person);
+student.grade = "11";
+student.getGrade = function() {
+    return this.grade;
+}
+const student2 = Object.create(student);
+   //put you object here
 
 /**
  * create new instance of student using Object.create
  */
-const student2 = //put you object here
-
+//const student2 = //put you object here
 module.exports = {
     person,
     student,
